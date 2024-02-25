0b0addc52cf3710e03006fd61ceaab35d561423f
sergey ostrovskiy <sergey.ostrovskiy@deploy.work>
2024-02-25 16:50:32 +0200

    Setup 'Home' screen.

--- mega_shop/android/app/src/main/kotlin/com/megashop/mega_shop/MainActivity.kt
+++ mega_shop/android/app/src/main/kotlin/com/megashop/mega_shop/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.megashop.mega_shop
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
---
+++ mega_shop/lib/common/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const appName = "Mega Shop";
+
+const textColorDarkCommon = Color(0xFF535353);
+const textColorLightCommon = Color(0xFFACACAC);
